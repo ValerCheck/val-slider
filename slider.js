5b74ec7e707f5b7b95126bef5360edb4dbba6df1
valercheck <mypowerismind@gmail.com>
2016-10-21 23:44:41 +0300

    Move coordinates from JS to HTML

--- slider.js
+++ slider.js
@@ -15,89 +15,22 @@ $(document).ready(function(){
 		title : 'Georgia State University 1950'
 	},{
 		set: 4,
-		points : [
-			{ data : [475,535.5,494,512,520,535.5,499.5,555] },
-			{ data : [525,535,555,498,585,521,555,558] },
-			{ data : [568,557,595.5,523,623,544,595.5,578] },
-			{ data : [517,615.5,526.5,603,543,615.5,534,628] }
-		],
 		title : 'Georgia State University Campus 1960'
 	},{
 		set: 5,
-		points : [
-			{ data : [546,433.6,562.6,415,589,438,572.3,456] },
-			{ data : [502,497,526.2,470,546,486.5,521,514] },
-			{ data : [534.8,581.3,551.2,564.1,563.2,575.4,546.7,592.9] },
-			{ data : [550.1,598.6,571.1,576.8,602.9,604.8,582.2,626.2] },
-			{ data : [589,630.7,609,608,625,623,604.5,645] },
-			{ data : [610,584.8,621.3,572,639,588.3,627.6,601] },
-			{ data : [628,569.9,643.5,552,668,574.5,652.8,592] },
-			{ data : [620,553.5,625.5,547,635,555.5,629.5,562] },
-			{ data : [666.9,637,701.3,599.3,714,611.6,679.7,649.2], fillColor : '000000'},
-			{ data : [686.9,655,721.7,617.3,734,629.6,700.1,667.2], fillColor : '000000'},
-			{ data : [727.5,605.5,750.5,580.5,762.9,593.5,740.8,617.6], fillColor : '000000'}
-		],
 		title : 'Georgia State University Campus 1970'
 	},{
 		set : 6,
-		imageUrl : 'https://netcommunity.gsu.edu/view.image?Id=3015',
-		points : [
-			{ data: [388.5,476.9,411,448.8,430.5,464,408.2,492.2] },
-			{ data: [487.5,454.8,510,426.8,529.5,442.2,507.8,470.2] },
-			{ data: [515.8,566.9,531.3,550.1,543.2,561,527.9,577.9] },
-			{ data: [604,519,615.5,505,635,522,623,536] },
-			{ data: [647,465.5,682.5,427,711,451.7,675.4,490] },
-			{ data: [748.6,392.6,748.6,371.4,763.8,371.4,763.8,392.6] },
-			{ data: [856,395,856,368,875,368,875,395] }
-		],
 		title : 'Georgia State University Campus 1980'
 	},{
 		set : 7,
-		points : [
-			{ data : [353.9,199,366.3,189.2,374.1,198,361.5,207.8] },
-			{ data : [364,225,383.7,208,397,222,377.1,239] },
-			{ data : [348.3,236.5,359,226.1,373.7,241.5,362,251.9] },
-			{ data : [308.2,338,323.5,324.4,334,335.5,318.5,349.6] },
-			{ data : [330.8,359.5,358.5,334.1,366.6,342.5,339,368] },
-			{ data : [292.1,387,305,372.8,324,389.5,311.5,403.2] },
-			{ data : [391.4,418,408,395,427.5,407,410,430] },
-			{ data : [438,448.5,456,426.7,487.1,449,469,472.3] },
-			{ data : [688,494,720.5,458,740,476.5,707,512], fillColor: '000000' },
-			{ data : [636,531,668,496,697,520,664,556] },
-			{ data : [471.9,576.5,490.1,553.2,523.1,580.8,504,603.8], fillColor : '000000'}
-		],
 		title : 'Georgia State University Campus 1990'
 	},{
 		set : 8,
-		points : [
-			{ data : [836,182,836,119,943,119,943,182] },
-			{ data : [375,257,405.5,229,426,252,395.5,280] },
-			{ data : [699.9,425.5,726.8,395.5,749.6,415,723.5,445.5] },
-			{ data : [728,450.5,756,419,773,435.5,745,466], fillColor: '000000' },
-			{ data : [655.5,596.5,705.8,539.9,725.1,558.7,676.7,615.2] }
-		],
 		title : 'Georgia State University Campus 2000-2009' 
 	},
 	{
 		set : 9,
-		points : [
-			{ data : [846,91,846,28,933,28,933,91] },
-			{ data : [493,254,493,202,544,202,544,254] },
-			{ data : [836,259,836,209,856,209,856,259] },
-			{ data : [857,287,857,216,886,216,886,287], fillColor : '000000' },
-			{ data : [498,305,498,264,546,264,546,305] },
-			{ data : [794,287,794,270,822,270,822,287] },
-			{ data : [460,386,460,317,496,317,496,386] },
-			{ data : [498,387,498,318,527,318,527,387] },
-			{ data : [529,386,529,318,583,318,583,386], fillColor : '000000' },
-			{ data : [767,382,767,356,776,356,776,382] },
-			{ data : [777,379,777,357,788,357,788,379] },
-			{ data : [765,395,765,382,791,382,791,392] },
-			{ data : [792,389,792,351,819,351,819,389] },
-			{ data : [973,396,973,363,993,363,993,396] },
-			{ data : [599,672.5,631,638,656,660,624,695] },
-			{ data : [830,915,830,823,921,823,921,915] }
-		],
 		title : 'Georgia State University Campus 2010-2015'
 	}];
 
@@ -119,12 +52,18 @@ $(document).ready(function(){
 		});
 	}
 
+	var Button = function(className) {
+		this.element = $("<input type='button'></input>").addClass(className);
+	}
+
 	var controls = 
 	$("<div class='slider-controls'></div>")
-	.append($("<input type='button' class='zoomout button'>"))
-	.append($("<input type='button' class='zoomin button active-btn' >"))
-	.append($("<input type='button' class='reset button active-btn' >"))
-	.append($("<input type='button' class='slideshow button active-btn'>"));
+	.append([
+		new Button('zoomout button').element,
+		new Button('zoomin button active-btn').element,
+		new Button('reset button active-btn').element,
+		new Button('slideshow button active-btn').element
+	]);
 
 	$('.slider-list >:first-child').addClass('active');
 
@@ -149,7 +88,12 @@ $(document).ready(function(){
 
 		areas.forEach(function(area,i){
 			var coords = $(area).attr('coords').split(',').map(parseFloat);
-			data[number].points.push({data:coords});
+			data[number].points.push({
+				data : coords/*,
+				title : $(area).data('tooltip-title'),
+				content : $(area).html()*/
+
+			});
 		});
 		$(areas).remove();
 	}
